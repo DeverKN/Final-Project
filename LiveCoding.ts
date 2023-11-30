cc420b6fd556b324e66c1b5695f80006e5f8145a
Kemme Nash, Dever Patrick <dpkemmen@iu.edu>
2023-11-29 23:08:56 -0500

    updated live demo

--- LiveCoding.ts
+++ LiveCoding.ts
@@ -1,6 +1,3 @@
-const last = (s: string) => s[s.length - 1];
-const first = (s: string) => s[0];
-
 /*
 
 Choose a word from each list such that the last letter of each word is the same as the first letter of the next
@@ -11,9 +8,13 @@ Choose a word from each list such that the last letter of each word is the same 
 
 */
 
+const last = (s: string) => s[s.length - 1];
+const first = (s: string) => s[0];
+const satisfies = (s: string, w: string) => last(s) === first(w);
+
 //  Naive solution
 
-const solution = () => {
+const naiveSolution = () => {
   const words1 = ["the", "that", "a"];
   const words2 = ["frog", "elephant", "thing"];
   const words3 = ["walked", "treaded", "grows"];
@@ -21,11 +22,11 @@ const solution = () => {
 
   for (const w1 of words1) {
     for (const w2 of words2) {
-      if (last(w1) === first(w2)) {
+      if (satisfies(w1, w2)) {
         for (const w3 of words3) {
-          if (last(w2) === first(w3)) {
+          if (satisfies(w2, w3)) {
             for (const w4 of words4) {
-              if (last(w3) === first(w4)) {
+              if (satisfies(w3, w4)) {
                 return `${w1} ${w2} ${w3} ${w4}`;
               }
             }
@@ -36,4 +37,36 @@ const solution = () => {
   }
 }
 
-console.log(solution());
+console.log(naiveSolution());
+
+
+const functionalSolution = (): string[] => {
+  const words = ["the", "that", "a"];
+  const words2 = ["frog", "elephant", "thing"];
+  const words3 = ["walked", "treaded", "grows"];
+  const words4 = ["slowly", "quickly"];
+
+  return words.flatMap((w1) =>
+    words2.flatMap((w2) => {
+      if (satisfies(w1, w2)) {
+        return words3.flatMap((w3) => {
+          if (satisfies(w2, w3)) {
+            return words4.flatMap((w4) => {
+              if (satisfies(w3, w4)) {
+                return [`${w1} ${w2} ${w3} ${w4}`];
+              } else {
+                return [];
+              }
+            });
+          } else {
+            return [];
+          }
+        });
+      } else {
+        return [];
+      }
+    })
+  );
+};
+
+console.log(functionalSolution());
